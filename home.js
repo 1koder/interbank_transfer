6bc120eb6e565d8eeabd61427c6d053c9a715bd2
1koder <aderohunbi.a.a@gmail.com>
2023-09-17 13:45:59 +0100

    Create home.js
    On-page JS

---
+++ home.js
@@ -0,0 +1,41 @@
+
+var b = 'smoothie';
+console.log(b);
+
+
+var someNumber = 45;
+console.log(someNumber);
+
+//var age = prompt('what is your age?');
+
+//document.getElementById("someText").innerHTML = age;
+
+//numbers in JS
+var num1 = 10;
+
+console.log(num1 / 4);
+// incrememnt or decrement by any number
+num1 += 15;
+console.log(num1);
+
+/*FUNCTIONS IN JS
+STEP 1: CREATE THE FUNCTION
+*/
+
+function fun() {
+    alert('my first function');
+}
+// STEP 2: CALL THE FUNCTION
+
+
+fun();
+
+function Welcome() {
+    var name = prompt('what is your name?');
+    var result = 'Welcome' + '' + name;
+    console.log(result);
+}
+
+Welcome();
+
+Welcome();
